cc73158152059f0afa9d7e5172e20ecf045a2b32
Sam <samg05981@gmail.com>
2022-10-05 04:00:47 +0400

    Calculator

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,39 +3,42 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var textResult: TextView
-    lateinit var textInput:TextView
-    lateinit var buttonNumber1: AppCompatButton
-    lateinit var buttonNumber2: AppCompatButton
-    lateinit var buttonNumber3: AppCompatButton
-    lateinit var buttonNumber4: AppCompatButton
-    lateinit var buttonNumber5: AppCompatButton
-    lateinit var buttonNumber6: AppCompatButton
-    lateinit var buttonNumber7: AppCompatButton
-    lateinit var buttonNumber8: AppCompatButton
-    lateinit var buttonNumber9: AppCompatButton
-    lateinit var buttonNumber0: AppCompatButton
-    lateinit var buttonNumber00: AppCompatButton
-    lateinit var buttonComma: AppCompatButton
-    lateinit var buttonClear: AppCompatButton
-    lateinit var buttonPercent: AppCompatButton
-    lateinit var buttonBack: AppCompatButton
-    lateinit var buttonDivide: AppCompatButton
-    lateinit var buttonMultiply: AppCompatButton
-    lateinit var buttonMinus: AppCompatButton
-    lateinit var buttonPlus: AppCompatButton
-    lateinit var buttonEqual: AppCompatButton
+
+    private lateinit var textResult: TextView
+    private lateinit var textInput: TextView
+    private lateinit var buttonNumber1: AppCompatButton
+    private lateinit var buttonNumber2: AppCompatButton
+    private lateinit var buttonNumber3: AppCompatButton
+    private lateinit var buttonNumber4: AppCompatButton
+    private lateinit var buttonNumber5: AppCompatButton
+    private lateinit var buttonNumber6: AppCompatButton
+    private lateinit var buttonNumber7: AppCompatButton
+    private lateinit var buttonNumber8: AppCompatButton
+    private lateinit var buttonNumber9: AppCompatButton
+    private lateinit var buttonNumber0: AppCompatButton
+    private lateinit var buttonNumber00: AppCompatButton
+    private lateinit var buttonComma: AppCompatButton
+    private lateinit var buttonClear: AppCompatButton
+    private lateinit var buttonPercent: AppCompatButton
+    private lateinit var buttonBack: AppCompatButton
+    private lateinit var buttonDivide: AppCompatButton
+    private lateinit var buttonMultiply: AppCompatButton
+    private lateinit var buttonMinus: AppCompatButton
+    private lateinit var buttonPlus: AppCompatButton
+    private lateinit var buttonEqual: AppCompatButton
+    private var buttonPressed: Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textResult = findViewById(R.id.textResult)
-textInput=findViewById(R.id.textInput)
+        textInput = findViewById(R.id.textInput)
         buttonNumber1 = findViewById(R.id.buttonNumber1)
         buttonNumber2 = findViewById(R.id.buttonNumber2)
         buttonNumber3 = findViewById(R.id.buttonNumber3)
@@ -58,89 +61,122 @@ textInput=findViewById(R.id.textInput)
         buttonPlus = findViewById(R.id.buttonPlus)
         buttonEqual = findViewById(R.id.buttonEqual)
 
-        buttonNumber1.setOnClickListener{set("1")}
-        buttonNumber2.setOnClickListener{set("2")}
-        buttonNumber3.setOnClickListener{set("3")}
-        buttonNumber4.setOnClickListener{set("4")}
-        buttonNumber5.setOnClickListener{set("5")}
-        buttonNumber6.setOnClickListener{set("6")}
-        buttonNumber7.setOnClickListener{set("7")}
-        buttonNumber8.setOnClickListener{set("8")}
-        buttonNumber9.setOnClickListener{set("9")}
-        buttonNumber00.setOnClickListener{set("00")}
-        buttonNumber0.setOnClickListener{set("0")}
-        buttonComma.setOnClickListener{set(".")}
-        buttonPercent.setOnClickListener{set("%")}
+        buttonNumber1.setOnClickListener { set("1") }
+        buttonNumber2.setOnClickListener { set("2") }
+        buttonNumber3.setOnClickListener { set("3") }
+        buttonNumber4.setOnClickListener { set("4") }
+        buttonNumber5.setOnClickListener { set("5") }
+        buttonNumber6.setOnClickListener { set("6") }
+        buttonNumber7.setOnClickListener { set("7") }
+        buttonNumber8.setOnClickListener { set("8") }
+        buttonNumber9.setOnClickListener { set("9") }
+        buttonNumber00.setOnClickListener { set("00") }
+        buttonNumber0.setOnClickListener { set("0") }
 
-        buttonPlus.setOnClickListener{ set("+")}
-        buttonMinus.setOnClickListener{set("-")}
-        buttonMultiply.setOnClickListener{set("*")}
-        buttonDivide.setOnClickListener{set("/")}
-
-        buttonClear.setOnClickListener{
-            textResult.text=""
-            textInput.text=""
+        buttonComma.setOnClickListener {
+            Toast.makeText(this, "still not working", Toast.LENGTH_SHORT).show()
+        }
+        buttonPercent.setOnClickListener {
+            Toast.makeText(this, "still not working", Toast.LENGTH_SHORT).show()
         }
 
-buttonEqual.setOnClickListener {
+        buttonPlus.setOnClickListener { set("+") }
+        buttonMinus.setOnClickListener { set("-") }
+        buttonMultiply.setOnClickListener { set("*") }
+        buttonDivide.setOnClickListener { set("/") }
 
-    if (textInput.text.toString().contains('*')) {
-        val r = multiply(textInput.text.toString())
-        textResult.text = r
-    }
-    if (textInput.text.toString().contains('-')) {
-        val r = minus(textInput.text.toString())
-        textResult.text = r
-    }
-    if (textInput.text.toString().contains('+')) {
-        val r = plus(textInput.text.toString())
-        textResult.text = r
-    }
-    if (textInput.text.toString().contains('/')) {
-        val r = divide(textInput.text.toString())
-        textResult.text = r
-    }
-}
-    buttonBack.setOnClickListener {
-        val number = textInput.text.toString()
-        if (number.isNotEmpty())
-            textInput.text = number.substring(0, number.length - 1)
 
-}
+
+        buttonClear.setOnClickListener {
+            textResult.text = ""
+            textInput.text = ""
         }
 
-    fun set(s:String){
+        buttonEqual.setOnClickListener {
+
+            if (textInput.text.toString().contains('*')) {
+                val r = multiply(textInput.text.toString())
+                textResult.text = r
+            }
+            if (textInput.text.toString().contains('-')) {
+                val r = minus(textInput.text.toString())
+                textResult.text = r
+            }
+            if (textInput.text.toString().contains('+')) {
+                val r = plus(textInput.text.toString())
+                textResult.text = r
+            }
+            if (textInput.text.toString().contains('/')) {
+                val r = divide(textInput.text.toString())
+                textResult.text = r
+            }
+        }
+        buttonBack.setOnClickListener {
+            val number = textInput.text.toString()
+            if (number.isNotEmpty())
+                textInput.text = number.substring(0, number.length - 1)
+
+
+        }
+    }
+
+    private fun set(s: String) {
+        checkUserInput(textInput, "*")
+        checkUserInput(textInput, "0")
+        checkUserInput(textInput, "+")
+        checkUserInput(textInput, "/")
+        checkUserInput(textInput, "-")
+
         textInput.append(s)
 
     }
-    private fun multiply (s: String): String {
 
-        val lastNumber=s.substringAfterLast("*")
-        val firstNumber=s.substringBeforeLast("*")
-       val resultMultiply= (lastNumber.toDouble()* firstNumber.toDouble()).toString()
+    private fun multiply(s: String): String {
+        val lastNumber = s.substringAfterLast("*")
+        val firstNumber = s.substringBeforeLast("*")
+        val resultMultiply = (lastNumber.toDouble() * firstNumber.toDouble()).toString()
         return resultMultiply
     }
-    private fun minus(s:String): String {
-        val lastNumber=s.substringAfterLast("-")
-        val firstNumber=s.substringBeforeLast("-")
-       val resultMinus= (firstNumber.toDouble()- lastNumber.toDouble()).toString()
+
+    private fun minus(s: String): String {
+        val lastNumber = s.substringAfterLast("-")
+        val firstNumber = s.substringBeforeLast("-")
+        val resultMinus = (firstNumber.toDouble() - lastNumber.toDouble()).toString()
         return resultMinus
     }
-    private fun divide(s:String):String{
-        val lastNumber=s.substringAfterLast("/")
-        val firstNumber=s.substringBeforeLast("/")
-        val resultDivide= (firstNumber.toDouble()/ lastNumber.toDouble()).toString()
+
+    private fun divide(s: String): String {
+        val lastNumber = s.substringAfterLast("/")
+        val firstNumber = s.substringBeforeLast("/")
+        val resultDivide = (firstNumber.toDouble() / lastNumber.toDouble()).toString()
         return resultDivide
     }
-    private fun plus(s:String):String{
-        val lastNumber=s.substringAfterLast("+")
-        val firstNumber=s.substringBeforeLast("+")
-        val resultPlus= (firstNumber.toDouble()+ lastNumber.toDouble()).toString()
+
+    private fun plus(s: String): String {
+        val lastNumber = s.substringAfterLast("+")
+        val firstNumber = s.substringBeforeLast("+")
+        val resultPlus = (firstNumber.toDouble() + lastNumber.toDouble()).toString()
         return resultPlus
     }
 
 
+    private fun checkUserInput(text1: TextView, value: String) {
+        val text = text1.text.toString()
+        var a = 1..9
+        if (text.take(1) == value) {
+            text1.text = null
+        }
+
+        if (text1.text.contains("//") || text1.text.contains("**") || text1.text.contains("++") || text1.text.contains(
+                "--"
+            )
+        ) {
+            textInput.text = text.substring(0, text.length - 1)
+        }
+    }
 }
+
+
 
 
 
